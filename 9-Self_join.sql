3450c5090c19a73dda4c9016d90d798d0dd83612
tiffangchan19 <49135446+tiffangchan19@users.noreply.github.com>
2019-07-01 09:34:18 +0800

    With the answer for the last question

--- 9-Self_join.sql
+++ 9-Self_join.sql
@@ -47,3 +47,12 @@ JOIN stops stopB ON stopB.id=b.stop
 WHERE a.stop=(SELECT id FROM stops WHERE name='Craiglockhart')
 
 /*10*/
+SELECT DISTINCT a.num, a.company, stopb.name, c.num,  c.company
+FROM route a JOIN route b
+ON (a.company = b.company AND a.num = b.num)
+JOIN ( route c JOIN route d ON (c.company = d.company AND c.num= d.num))
+JOIN stops stopa ON (a.stop = stopa.id)
+JOIN stops stopb ON (b.stop = stopb.id)
+JOIN stops stopc ON (c.stop = stopc.id)
+JOIN stops stopd ON (d.stop = stopd.id)
+WHERE  stopa.name = 'Craiglockhart' AND stopd.name = 'Lochend' AND  stopb.name = stopc.name
